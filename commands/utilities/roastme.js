6f3dfd3c1c7ea76da548988128bf3b54d86e456c
Tafish <tafish2015@gmail.com>
2023-12-20 11:23:25 +0100

    add

--- commands/utilities/roastme.js
+++ commands/utilities/roastme.js
@@ -2,37 +2,12 @@ const {
     SlashCommandBuilder
 } = require('discord.js');
 
-const roast = [{
-        roast: "Ik zou je willen roasten, maar dat zou oneerlijk zijn tegenover vuilnis."
-    },
-    {
-        roast: "Als dom zijn een olympische sport was, zou jij zeker een gouden medaille winnen. "
-    },
-    {
-        roast: "Je intelligentie is als een horizon: het lijkt ver weg"
-    },
-    {
-        roast: "Als je zo traag bent in het begrijpen van dingen, moet je voorzichtig zijn dat je niet door slakken wordt ingehaald."
-    },
-    {
-        roast: "Als intelligentie valuta was, zou je zeker failliet zijn"
-    },
-    {
-        roast: "Je bent als een onopgelost Rubiks kubus - kleurloos en verwarrend voor iedereen."
-    },
-    {
-        roast: "Je bent als een software-update: niemand wil je, maar je blijft maar vragen om geïnstalleerd te worden."
-    },
-    {
-        roast: "Als ik je een cent zou geven voor je gedachten, zou ik verandering terugkrijgen"
-    },
-    {
-        roast: "Je zelfvertrouwen is als een verboden snelweg - er is niemand die erop mag."
-    },
-    {
-        roast: "Als je brein een spel was, zou het zeker verstoppertje spelen, want niemand kan het vinden."
-    }
+const roast = [
+    "Je intelligentie is zo zeldzaam als een witte olifant in de sneeuw.",
+    "Als domheid een beroep was, zou jij directeur zijn.",
+    "Je bent als een gloeilamp: aan de buitenkant ben je helder, maar van binnen ben je leeg.",
 ];
+
 
 module.exports = {
     data: new SlashCommandBuilder()
